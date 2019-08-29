ca6b3d80875f02ae6f840cecb8f7dcf0f102699a
nowstring <ljhhyun99@gmail.com>
2019-08-29 23:02:16 +0900

    Inital commit

---
+++ src/ScalaExample2.scala
@@ -0,0 +1,35 @@
+object ScalaExample2 {
+  def start: Unit = {
+    println(fibonacci(5))
+  }
+
+  def fibonacci(target: Int): Int = {
+    @annotation.tailrec
+    def go(result: Int, x: Int, y: Int): Int = {
+      if (result == target) x
+      else go(result + 1, y, x+y)
+    }
+    go(0, 0, 1)
+  }
+
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    def loop(n: Int): Boolean = {
+      if (n >= as.length - 1) true
+      else if (ordered(as(n),as(n+1))) loop(n+1)
+      else false
+    }
+    loop(0)
+  }
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = {
+    (a: A) => ((b: B) => f(a, b))
+  }
+
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = {
+    (a: A, b: B) => f(a)(b)
+  }
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C = {
+    (a: A) => f(g(a))
+  }
+}
---
+++ src/ScalaFunctionalProgramming.scala
@@ -0,0 +1,26 @@
+object ScalaFunctionalProgramming {
+  def main(args: Array[String]): Unit = start()
+
+  def abs(n: Int): Int = {
+    if (n < 0) -n
+    else n
+  }
+
+  def factorial(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, acc: Int): Int = {
+      if (n <= 0) acc
+      else go(n - 1, n * acc)
+    }
+    go(n, 1)
+  }
+
+  private def formatResult(name: String, n: Int, f: Int => Int) = {
+    val msg = "The %s of %d is %d."
+    msg.format(name, n, f(n))
+  }
+
+  def start(): Unit = {
+    ScalaExample2.start
+  }
+}
