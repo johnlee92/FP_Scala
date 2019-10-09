a8c5230704183f12f34a25c82466c00796884d51
nowstring <ljhhyun99@gmail.com>
2019-10-09 23:49:56 +0900

    Add Example 6.11

--- src/ScalaExample6.scala
+++ src/ScalaExample6.scala
@@ -1,4 +1,5 @@
 case object ScalaExample6 {
+
   trait RNG {
     def nextInt: (Int, RNG)
   }
@@ -11,9 +12,10 @@ case object ScalaExample6 {
       (n, nextRNG)
     }
   }
+
   // Example 6.1
   def nonNegativeInt(rng: RNG): (Int, RNG) = rng.nextInt match {
-    case (i, rng) if(i < 0 || i == Int.MinValue) => (-i, rng.nextInt._2)
+    case (i, rng) if (i < 0 || i == Int.MinValue) => (-i, rng.nextInt._2)
     case _ => rng.nextInt
   }
 
@@ -43,10 +45,11 @@ case object ScalaExample6 {
 
   // Example 6.4
   def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
-    def loop(count: Int)(list: List[Int], rng: RNG): (List[Int], RNG)  = {
+    def loop(count: Int)(list: List[Int], rng: RNG): (List[Int], RNG) = {
       if (count == 0) (list, rng)
       else loop(count - 1)(rng.nextInt._1 :: list, rng.nextInt._2)
     }
+
     loop(count)(List(), rng)
   }
 
@@ -84,7 +87,7 @@ case object ScalaExample6 {
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
     rng => {
       val list = List()
-      fs.foldRight((Nil:List[A], rng))((rand, result) => (rand(result._2)._1 :: result._1, rand(result._2)._2))
+      fs.foldRight((Nil: List[A], rng))((rand, result) => (rand(result._2)._1 :: result._1, rand(result._2)._2))
     }
 
   // Example 6.8
@@ -135,7 +138,87 @@ case object ScalaExample6 {
     def sequence[S, A](sl: List[State[S, A]]): State[S, List[A]] =
       State(run = s => {
         val list = List()
-        sl.foldRight((Nil:List[A], s))((state, result) => (state.run(s)._1 :: result._1, state.run(s)._2))
+        sl.foldRight((Nil: List[A], s))((state, result) => (state.run(s)._1 :: result._1, state.run(s)._2))
       })
   }
+
+  def get[S]: State[S, S] = State(s => (s, s))
+
+  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+
+  def modify[S](f: S => S): State[S, Unit] = for {
+    s <- get
+    _ <- set(f(s))
+  } yield ()
+
+  // Example 6.11
+  sealed trait Input
+
+  case object Coin extends Input
+
+  case object Turn extends Input
+
+  case class Machine(locked: Boolean, candies: Int, coins: Int)
+
+  def setLocked(b: Boolean): Machine => Machine = machine => {
+    Machine(
+      locked = b,
+      candies = machine.candies,
+      coins = machine.coins
+    )
+  }
+
+  def increaseCoins(): Machine => Machine = machine => {
+    Machine(
+      locked = machine.locked,
+      candies = machine.candies,
+      coins = machine.coins + 1
+    )
+  }
+
+  def decreaseCandies(): Machine => Machine = machine => {
+    Machine(
+      locked = machine.locked,
+      candies = machine.candies - 1,
+      coins = machine.coins
+    )
+  }
+
+  def mutate(input: Input, state: Machine): Machine = {
+    if (state.candies < 1) state
+    input match {
+      case Coin => {
+        val machine1 = modify(increaseCoins()).run(state)._2
+        if (!state.locked) machine1
+        val machine2 = modify(setLocked(false)).run(machine1)._2
+        machine2
+      }
+
+      case Turn => {
+        if (state.locked) state
+        val machine1 = modify(decreaseCandies()).run(state)._2
+        val machine2 = modify(setLocked(true)).run(machine1)._2
+        machine2
+      }
+    }
+  }
+
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = State { (machine: Machine) => {
+    def loop(inputs: List[Input], state: Machine): ((Int, Int), Machine) = {
+      inputs match {
+        case input :: t => {
+          val newState = mutate(input, state)
+          loop(t, newState)
+        }
+        case nil => ((state.coins, state.candies), state)
+      }
+    }
+
+    loop(inputs, machine)
+    }
+  }
+
+  val machine = Machine(locked = true, candies = 10, coins = 0)
+  val inputs: List[Input] = List.apply(Coin, Turn, Coin, Turn, Coin)
+  print(simulateMachine(inputs).run(machine)._1)
 }
